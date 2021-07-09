709d411e67d3ac3f4fb2b813300bcc3843b094fb
André Carlos Jr <andre-carlos@live.com>
2021-07-09 10:16:57 -0300

    - Exporting from index

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,4 @@
+import type { ISpotifyAlbum, ISpotifyPlaylist } from './spotifyTypes';
 export interface TokenSpotify {
   access_token: string;
   token_type: string;
@@ -6,50 +7,17 @@ export interface TokenSpotify {
 }
 
 export interface PlaylistItems {
-  name: "";
+  name: '';
   image: {};
-  href: "";
-  id: "";
+  href: '';
+  id: '';
   icons: [
     {
-      url: "";
+      url: '';
       height: number;
       width: number;
     }
   ];
 }
 
-export interface ISpotifyAlbum {
-  album_type: "album";
-  artists: [
-    {
-      external_urls: {
-        spotify: "https://open.spotify.com/artist/6AgTAQt8XS6jRWi4sX7w49";
-      };
-      href: "https://api.spotify.com/v1/artists/6AgTAQt8XS6jRWi4sX7w49";
-      id: "6AgTAQt8XS6jRWi4sX7w49";
-      name: "Polo G";
-      type: "artist";
-      uri: "spotify:artist:6AgTAQt8XS6jRWi4sX7w49";
-    }
-  ];
-  available_markets: [];
-  external_urls: {
-    spotify: "https://open.spotify.com/album/7KSf6p0G0mZd7j60etVTwT";
-  };
-  href: "https://api.spotify.com/v1/albums/7KSf6p0G0mZd7j60etVTwT";
-  id: "7KSf6p0G0mZd7j60etVTwT";
-  images: [
-    {
-      height: 640;
-      url: "https://i.scdn.co/image/ab67616d0000b273a493e05c99d8ec5e8020ff2b";
-      width: 640;
-    }
-  ];
-  name: "Hall of Fame";
-  release_date: "2021-06-11";
-  release_date_precision: "day";
-  total_tracks: 20;
-  type: "album";
-  uri: "spotify:album:7KSf6p0G0mZd7j60etVTwT";
-}
+export { ISpotifyAlbum, ISpotifyPlaylist };
--- types/spotifyTypes.d.ts
+++ types/spotifyTypes.d.ts
@@ -22,6 +22,41 @@ export interface ISpotifyPlaylist {
     href: string;
     total: number;
   };
-  type: "playlist";
+  type: 'playlist';
   uri: string;
+}
+
+export interface ISpotifyAlbum {
+  album_type: 'album';
+  artists: [
+    {
+      external_urls: {
+        spotify: 'https://open.spotify.com/artist/6AgTAQt8XS6jRWi4sX7w49';
+      };
+      href: 'https://api.spotify.com/v1/artists/6AgTAQt8XS6jRWi4sX7w49';
+      id: '6AgTAQt8XS6jRWi4sX7w49';
+      name: 'Polo G';
+      type: 'artist';
+      uri: 'spotify:artist:6AgTAQt8XS6jRWi4sX7w49';
+    }
+  ];
+  available_markets: [];
+  external_urls: {
+    spotify: 'https://open.spotify.com/album/7KSf6p0G0mZd7j60etVTwT';
+  };
+  href: 'https://api.spotify.com/v1/albums/7KSf6p0G0mZd7j60etVTwT';
+  id: '7KSf6p0G0mZd7j60etVTwT';
+  images: [
+    {
+      height: 640;
+      url: 'https://i.scdn.co/image/ab67616d0000b273a493e05c99d8ec5e8020ff2b';
+      width: 640;
+    }
+  ];
+  name: 'Hall of Fame';
+  release_date: '2021-06-11';
+  release_date_precision: 'day';
+  total_tracks: 20;
+  type: 'album';
+  uri: 'spotify:album:7KSf6p0G0mZd7j60etVTwT';
 }
