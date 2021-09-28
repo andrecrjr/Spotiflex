dfdd33f8ec473a7d0568f9c18403cf8fa54cab77
André Carlos Jr <andre-carlos@live.com>
2021-09-27 21:44:18 -0300

    -removendo queue wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import TransactionPage from '../components/TransactionPage';
 import Layout from '../components/Layout';
 import '../styles/globals.scss';
-import { UserPlaylistWrapper } from '../components/contextWrappers/UserPlaylist';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
