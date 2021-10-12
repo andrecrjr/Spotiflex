1146a5cf0702873e046702ccfaa7f3a972ae1fb6
André Carlos Jr <andre-carlos@live.com>
2021-10-12 12:58:40 -0300

    - adicionando seo

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -4,6 +4,7 @@ import { ISearchSpotify } from '../../types/spotifyTypes';
 import { getDataSpotify } from '../../components/services';
 import Carousel from '../../components/Layout/List/Carousel';
 import SongList from '../../components/Layout/List';
+import LayoutMetaSEO from '../../components/Layout/LayoutMetaSEO';
 
 const Search: React.FC<{ resp: ISearchSpotify; query: string; notFound }> = ({
   resp,
@@ -19,26 +20,29 @@ const Search: React.FC<{ resp: ISearchSpotify; query: string; notFound }> = ({
   };
   if (query.length > 0)
     return (
-      <div className='block' id='explorer'>
-        {Object.keys(autoComplete).length > 0 && (
-          <>
-            {autoComplete.artists.items.length > 0 && (
-              <SongList
-                listType={{ artists: autoComplete.artists.items }}
-                iconsWithTitle={true}
-                name={`Artists found with "${query}"`}
-              />
-            )}
-            {autoComplete.albums.items.length > 0 && (
-              <SongList
-                listType={{ albums: autoComplete.albums.items }}
-                iconsWithTitle={true}
-                name={`Albums found with "${query}"`}
-              />
-            )}
-          </>
-        )}
-      </div>
+      <>
+        <LayoutMetaSEO title={`Searching by ${query}`} />
+        <div className='block' id='explorer'>
+          {Object.keys(autoComplete).length > 0 && (
+            <>
+              {autoComplete.artists.items.length > 0 && (
+                <SongList
+                  listType={{ artists: autoComplete.artists.items }}
+                  iconsWithTitle={true}
+                  name={`Artists found with "${query}"`}
+                />
+              )}
+              {autoComplete.albums.items.length > 0 && (
+                <SongList
+                  listType={{ albums: autoComplete.albums.items }}
+                  iconsWithTitle={true}
+                  name={`Albums found with "${query}"`}
+                />
+              )}
+            </>
+          )}
+        </div>
+      </>
     );
   return (
     <p>No searchings, please search your favorite song, album or artist!</p>
