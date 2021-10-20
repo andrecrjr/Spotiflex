1c6aaf975794b350fc0f97bdc35d04424066eee6
André Carlos Jr <andre-carlos@live.com>
2021-10-19 21:09:37 -0300

    - adicionando arrow carousel

--- components/Layout/List/Carousel.tsx
+++ components/Layout/List/Carousel.tsx
@@ -3,7 +3,7 @@ import { PlaylistItems, ISpotifyAlbum } from '../../../types';
 import { IArtistSpotify } from '../../../types/spotifyTypes';
 import CarouselButton from './CarouselButton';
 import { GeneralPlaylist, GeneralAlbum } from './Playlist';
-
+import { CgArrowLeftO, CgArrowRightO } from 'react-icons/cg';
 const Carousel: React.FC<{
   listType: {
     playlists?: PlaylistItems[];
@@ -90,22 +90,10 @@ const Carousel: React.FC<{
           </ul>
           <div className='slider__wrapper--controls'>
             <CarouselButton ref={nextStep} controlSide={'next'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowRightO />
             </CarouselButton>
             <CarouselButton ref={previousStep} controlSide={'previous'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowLeftO />
             </CarouselButton>
           </div>
         </>
@@ -124,22 +112,10 @@ const Carousel: React.FC<{
           </ul>
           <div className='slider__wrapper--controls'>
             <CarouselButton ref={nextStep} controlSide={'next'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowRightO />
             </CarouselButton>
             <CarouselButton ref={previousStep} controlSide={'previous'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowLeftO />
             </CarouselButton>
           </div>
         </>
@@ -158,22 +134,10 @@ const Carousel: React.FC<{
           </ul>
           <div className='slider__wrapper--controls'>
             <CarouselButton ref={nextStep} controlSide={'next'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowRightO />
             </CarouselButton>
             <CarouselButton ref={previousStep} controlSide={'previous'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='icon'
-                viewBox='0 0 32 32'
-              >
-                <path d='M28 14H8.8l4.62-4.62c.4-.4.58-.86.58-1.38 0-.98-.81-2-2-2-.53 0-1 .2-1.38.58l-7.96 7.96c-.33.33-.66.73-.66 1.46s.28 1.08.65 1.45l7.97 7.97c.39.39.85.58 1.38.58 1.19 0 2-1.02 2-2 0-.52-.19-.99-.58-1.38L8.8 18H28a2 2 0 000-4z' />
-              </svg>
+              <CgArrowLeftO />
             </CarouselButton>
           </div>
         </>
