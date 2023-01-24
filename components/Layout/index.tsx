95b087d4b0b029b131293eb38c6a35784c426dca
André Carlos Jr <andre-carlos@live.com>
2023-01-23 21:05:06 -0300

    - fix unused var

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Header from './Header';
 import { TrackPlayerFooter } from './Tracklist/TrackPlayerFooter';
+import Script from 'next/script';
 
 const Layout: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
     <>
+      {process.env.NODE_ENV !== 'development' && (
+        <>
+          <Script
+            async
+            src='https://www.googletagmanager.com/gtag/js?id=G-V00R8HQRD3'
+            strategy='afterInteractive'
+          ></Script>
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {` window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-V00R8HQRD3');`}
+          </Script>
+        </>
+      )}
+
       <main className={`main--grid`}>
         <Header />
         <section className='main--wrapper'>
