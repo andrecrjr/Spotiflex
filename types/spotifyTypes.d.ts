7b652dfe68cb0b2db7b6c8f023ca3e4790a1687a
André Carlos Jr <andre-carlos@live.com>
2023-01-31 22:00:36 -0300

    - fixing types

--- types/spotifyTypes.d.ts
+++ types/spotifyTypes.d.ts
@@ -1,120 +1,8 @@
-import {
-  ISpotifyAlbum,
-  ISpotifyPlaylist,
-  ISpotifyTopTrack,
-} from './spotifyTypes.d';
-
 type ImageObject = {
   height: number | unknown;
   url: string;
   width: number | unknown;
 };
-
-export type typeOfTracklist =
-  | 'album'
-  | 'playlist'
-  | 'single'
-  | 'podcast'
-  | 'track'
-  | 'tracklist';
-
-export interface Track {
-  artists?: {
-    external_urls?: {
-      spotify?: string;
-    };
-    href?: string;
-    id?: string;
-    name?: string;
-    type?: string;
-    uri?: string;
-  }[];
-  album?: Record<string, unknown>;
-  added_at?: string;
-  added_by?: {
-    external_urls?: {
-      spotify: string;
-    };
-    href?: string;
-    id?: string;
-    type?: string;
-    uri?: string;
-  };
-  is_local?: boolean;
-  primary_color?: string;
-  album?: ISpotifyAlbum;
-  available_markets?: string[];
-  disc_number?: number;
-  duration_ms?: number;
-  episode?: boolean;
-  explicit?: boolean;
-  href?: string;
-  id?: string;
-  is_local?: boolean;
-  name?: string;
-  popularity?: number;
-  preview_url?: string;
-  track_number?: number;
-  track?: Track;
-  type?: string;
-  uri?: string;
-}
-
-interface InfoTrack extends Track {
-  track?: boolean;
-}
-
-interface PlaylistTracks {
-  items?: Track[];
-  limit?: number;
-  next?: number | unknown;
-  offset?: number | unknown;
-  previous?: number | unknown;
-  total?: number | unknown;
-}
-
-export type ISpotifyPlaylistWrapper = {
-  playlists: {
-    href: string;
-    items: ISpotifyPlaylist[];
-  };
-};
-
-export interface ISpotifyPlaylist {
-  collaborative?: boolean;
-  description?: string;
-  external_urls?: {
-    spotify?: string;
-  };
-  href?: string;
-  id?: string;
-  images?: ImageObject[];
-  name?: string;
-  owner?: {
-    display_name: string;
-    href: string;
-    id: string;
-    type: string;
-    uri: string;
-  };
-  primary_color?: string;
-  public?: boolean;
-  snapshot_id?: string;
-  tracks?: PlaylistTracks;
-  type?: string;
-  uri?: string;
-  followers?: {
-    href: string | unknown;
-    total: number;
-  };
-  video_thumbnail?: Record<string, unknown>;
-}
-
-export type ISpotifyAllTrackList =
-  | ISpotifyAlbum
-  | ISpotifyPlaylist
-  | ISpotifyTopTrack;
-
 export interface ISpotifyAlbum {
   album_type?: string;
   artists?: {
@@ -147,6 +35,111 @@ export interface ISpotifyAlbum {
   type?: string;
   uri?: string;
 }
+
+export interface Track {
+  artists?: {
+    external_urls?: {
+      spotify?: string;
+    };
+    href?: string;
+    id?: string;
+    name?: string;
+    type?: string;
+    uri?: string;
+  }[];
+  added_at?: string;
+  added_by?: {
+    external_urls?: {
+      spotify: string;
+    };
+    href?: string;
+    id?: string;
+    type?: string;
+    uri?: string;
+  };
+  primary_color?: string;
+  album?: ISpotifyAlbum;
+  available_markets?: string[];
+  disc_number?: number;
+  duration_ms?: number;
+  episode?: boolean;
+  explicit?: boolean;
+  href?: string;
+  id?: string;
+  is_local?: boolean;
+  name?: string;
+  popularity?: number;
+  preview_url?: string;
+  track_number?: number;
+  track?: Track;
+  type?: string;
+  uri?: string;
+}
+
+export interface ISpotifyPlaylist {
+  collaborative?: boolean;
+  description?: string;
+  external_urls?: {
+    spotify?: string;
+  };
+  href?: string;
+  id?: string;
+  images?: ImageObject[];
+  name?: string;
+  owner?: {
+    display_name: string;
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  primary_color?: string;
+  public?: boolean;
+  snapshot_id?: string;
+  tracks?: PlaylistTracks;
+  type?: string;
+  uri?: string;
+  followers?: {
+    href: string | unknown;
+    total: number;
+  };
+  video_thumbnail?: Record<string, unknown>;
+}
+
+export interface ISpotifyTopTrack {
+  tracks: Track[];
+  items?: [];
+  type?: 'tracklist';
+}
+
+export type typeOfTracklist =
+  | 'album'
+  | 'playlist'
+  | 'single'
+  | 'podcast'
+  | 'track'
+  | 'tracklist';
+
+interface PlaylistTracks {
+  items?: Track[];
+  limit?: number;
+  next?: number | unknown;
+  offset?: number | unknown;
+  previous?: number | unknown;
+  total?: number | unknown;
+}
+
+export type ISpotifyPlaylistWrapper = {
+  playlists: {
+    href: string;
+    items: ISpotifyPlaylist[];
+  };
+};
+
+export type ISpotifyAllTrackList =
+  | ISpotifyAlbum
+  | ISpotifyPlaylist
+  | ISpotifyTopTrack;
 
 export interface ISpotifyArtist {
   external_urls?: {
@@ -198,18 +191,7 @@ export interface ITracklist {
   name: 'Raised on Country';
   preview_url: 'https://p.scdn.co/mp3-preview/b701335497d8bb45ea6b79e2d5a16c541789240c?cid=774b29d4f13844c495f206cafdad9c86';
   track_number: 1;
-  items?: Tracks[];
+  items?: Track[];
   type: 'track';
   uri: 'spotify:track:5fBCiTK3cN8RJb7AlfwXfB';
-}
-
-type PropsGenre = {
-  items: ISpotifyPlaylist[];
-  title?: string;
-};
-
-export interface ISpotifyTopTrack {
-  tracks: Tracks[];
-  items?: [];
-  type?: 'tracklist';
 }
