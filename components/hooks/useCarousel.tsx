b85581b7db2dba1e4984f05aabde5a511a9145ac
André Carlos Jr <andre-carlos@live.com>
2023-02-28 00:20:19 -0300

    - update logic to carousel

--- components/hooks/useCarousel.tsx
+++ components/hooks/useCarousel.tsx
@@ -1,70 +1,91 @@
 import { useRef, useEffect, MutableRefObject } from 'react';
 
-export const useCarousel = (): {
+type CarouselRefs = {
   block_pane: MutableRefObject<HTMLUListElement>;
   nextStep: MutableRefObject<HTMLButtonElement>;
   previousStep: MutableRefObject<HTMLButtonElement>;
-} => {
+};
+
+export const useCarousel = (): CarouselRefs => {
   const block_pane = useRef<HTMLUListElement>();
   const nextStep = useRef<HTMLButtonElement>();
   const previousStep = useRef<HTMLButtonElement>();
-
-  const width = useRef(0);
+  const width = useRef<number>(0);
 
   useEffect(() => {
-    const scrollWidth = block_pane.current?.scrollWidth;
-    const windowPane = block_pane.current?.clientWidth;
+    const scrollWidth = block_pane.current?.scrollWidth ?? 0;
+    const windowPane = block_pane.current?.clientWidth ?? 0;
     const carouselChildrenLength =
-      block_pane.current?.firstChild.childNodes.length;
-    const sizeOfChildren = scrollWidth / carouselChildrenLength;
+      block_pane.current?.firstChild?.childNodes.length ?? 0;
+    const sizeOfChildren =
+      carouselChildrenLength > 0 ? scrollWidth / carouselChildrenLength : 0;
     const totalCarouselChildrenLength = sizeOfChildren * carouselChildrenLength;
     const nextPath = windowPane / sizeOfChildren;
 
-    const { current: divCarousel } = block_pane;
+    const divCarousel = block_pane.current
+      ?.children as HTMLCollectionOf<HTMLElement>;
 
-    const blockPane = divCarousel?.children as HTMLCollectionOf<HTMLElement>;
+    const setButtonOpacity = (
+      button: HTMLButtonElement | undefined,
+      opacity: number
+    ) => {
+      if (button) {
+        button.style.opacity = opacity.toString();
+        button.style.pointerEvents = opacity === 1 ? 'visible' : 'none';
+        button.style.cursor = opacity === 1 ? 'pointer' : 'unset';
+      }
+    };
 
-    if (previousStep.current && width.current === 0) {
-      previousStep.current.style.opacity = '0.1';
-    }
-
-    nextStep.current?.addEventListener('click', (e) => {
+    const handleNextClick = (e: MouseEvent) => {
       e.preventDefault();
       if (scrollWidth - windowPane >= -totalCarouselChildrenLength) {
         const newWidth = width.current - sizeOfChildren * Math.floor(nextPath);
-        blockPane[0].style.transform = `translateX(${newWidth}px)`;
+        divCarousel[0].style.transform = `translateX(${newWidth}px)`;
         width.current = newWidth;
         if (Math.abs(newWidth) >= scrollWidth - windowPane) {
-          nextStep.current.style.opacity = '0.1';
-          nextStep.current.style.pointerEvents = 'none';
-          nextStep.current.style.cursor = 'unset';
-          previousStep.current.style.opacity = '1';
+          setButtonOpacity(nextStep.current, 0.1);
+          setButtonOpacity(previousStep.current, 1);
         } else {
-          previousStep.current.style.opacity = '1';
-          nextStep.current.style.pointerEvents = 'visible';
-          nextStep.current.style.cursor = 'pointer';
+          setButtonOpacity(nextStep.current, 1);
+          setButtonOpacity(previousStep.current, 1);
         }
       }
-    });
+    };
 
-    previousStep.current?.addEventListener('click', (e) => {
+    const handlePreviousClick = (e: MouseEvent) => {
       e.preventDefault();
       if (scrollWidth + windowPane > width.current && width.current !== 0) {
         width.current = width.current + sizeOfChildren * Math.floor(nextPath);
-        blockPane[0].style.transform = `translateX(${width.current}px)`;
+        divCarousel[0].style.transform = `translateX(${width.current}px)`;
         if (Math.abs(width.current) > 0) {
-          previousStep.current.style.opacity = '1';
-          nextStep.current.style.opacity = '1';
-          nextStep.current.style.pointerEvents = 'visible';
-          nextStep.current.style.cursor = 'pointer';
-        } else {
-          nextStep.current.style.opacity = '1';
-          previousStep.current.style.opacity = '0.1';
-          nextStep.current.style.pointerEvents = 'visible';
+          setButtonOpacity(nextStep.current, 1);
+          setButtonOpacity(previousStep.current, 1);
+        }
+        if (width.current === 0) {
+          setButtonOpacity(nextStep.current, 1);
+          setButtonOpacity(previousStep.current, 0.1);
         }
       }
-    });
+    };
+
+    setButtonOpacity(previousStep.current, 0.1);
+    setButtonOpacity(nextStep.current, 1);
+
+    if (scrollWidth <= windowPane) {
+      setButtonOpacity(nextStep.current, 0.1);
+    }
+    if (nextStep.current) {
+      nextStep.current.addEventListener('click', handleNextClick);
+    }
+    if (previousStep.current) {
+      previousStep.current.addEventListener('click', handlePreviousClick);
+    }
   }, []);
 
-  return { block_pane, nextStep, previousStep };
+  const carouselRefs: CarouselRefs = {
+    block_pane,
+    nextStep,
+    previousStep,
+  };
+  return carouselRefs;
 };
